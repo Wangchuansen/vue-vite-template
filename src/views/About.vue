a9557042b31bd07e0a4422af0d127f7e5a24bb48
wcs <2824762249@qq.com>
2022-12-01 19:41:56 +0800

    fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,21 @@
 <template>
   <div>
     <el-button type="primary" @click="test">about</el-button>
+    <p>{{str}}</p>
   </div>
 </template>
 
 
 <script setup>
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import service from "../api/index";
-
+let str = ref()
   const test = () => {
     return service({
       url: "dream-demo/demo",
       method: "get",
     }).then((res) => {
-      console.log(res)
+      str.value = res.data
     });
   }
 
